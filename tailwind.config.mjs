a652879edf620efae8da40988bc881ba81946aae
RaksoFox <raksofox@vivaldi.net>
2024-09-09 15:15:32 +0200

    chore: add some basic components for landing page

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -1,8 +1,15 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
-	theme: {
-		extend: {},
-	},
-	plugins: [],
+    content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
+    theme: {
+        extend: {
+            colors: {
+                owopurple: {
+                    500: '#523c61',
+                    800: '#24162f'
+                }
+            }
+        }
+    },
+    plugins: [],
 }
